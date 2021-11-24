f70ca9ce8e4f851066208ac278c10c75e270a31d
kikim <85283385+gs-kikim@users.noreply.github.com>
2021-11-24 19:06:48 +0900

    Update Q6.py

--- src/greedy/Q6.py
+++ src/greedy/Q6.py
@@ -1,11 +1,25 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42891
-def greedy_Q6(food_times, k) -> int:
-    index = -1
-    n = len(food_times)
-    for i in range(0, k):
-        index = i % n
-        if food_times[index] is 0:
-            continue
-        food_times[index] = food_times[index]-1
+import heapq
 
-    return -1 if sum(food_times) is 0 else index
+
+def greedy_Q6(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+
+    q = []
+    for x, y in enumerate(food_times):
+        heapq.heappush(q, (y, x + 1))
+
+    food_count = len(food_times)
+    prev = 0
+    while True:
+        num, idx = heapq.heappop(q)
+        if (num - prev) * food_count <= k:
+            k -= ((num - prev) * food_count)
+            food_count -= 1
+            prev = num
+        else:
+            heapq.heappush(q, (num, idx))
+            break
+    q = sorted(q, key=lambda x: x[1])
+    return q[k % food_count][1]
