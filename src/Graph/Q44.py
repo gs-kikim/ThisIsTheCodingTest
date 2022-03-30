a87e1492ad90abdae93b4c0e7235fb57c8227542
gs-kikim <kikim@genians.com>
2022-03-30 20:29:42 +0900

    q43-44 GRAPH commit

---
+++ src/Graph/Q44.py
@@ -0,0 +1,50 @@
+"""
+- 서로소 : 공통 원소가 없는 두 집함. 자신이 속한 집합을 찾을 때, 루트 노드를 재귀적으로 찾습니다.
+- 신장트리 : 하나의 그래프가 있을때 모든 노드를 포함하는 부분 그래프
+- 크루스칼 알고리즘 : 가능한 초소 비용의 신장 트리를 찾아주는 알고리즘
+- 위상 정렬 알고리즘 : 방향 그래프의 모든 노드들을 방향성에 거스르지 않도록 순서대로 나열하는 정렬 기법.
+https://www.acmicpc.net/problem/2887
+"""
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+def solution(N, MAP):
+    parent = [0] * (N + 1)
+    edges = []
+    result = 0
+
+    for i in range(1, N + 1):
+        parent[i] = i
+
+    for i in range(N - 1):
+        a = i
+        b = i + 1
+        xa, ya, za = MAP[a]
+        xb, yb, zb = MAP[b]
+        cost = min([abs(xa - xb), abs(ya-xb), abs(za - zb)])
+        edges.append((cost, a, b))
+
+    edges.sort()  # 비용순 정렬
+
+    for edge in edges:
+        cost, a, b = edge
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            result += cost
+
+    return result
--- test/test_Graph.py
+++ test/test_Graph.py
@@ -1,14 +1,14 @@
 import unittest
 
-from src.Graph import Q41, Q42
+from src.Graph import Q41, Q42, Q43, Q44
 
 
 class TestGraph(unittest.TestCase):
     def test_Q41(self):
         self.assertEqual(Q41.solution(5, 4,
                                       [[0, 1, 0, 1, 1],
-                                       [1, 0, 1, 1, 0],
-                                       [0, 1, 0, 0, 0],
+                                       [1, 0, 0, 1, 0],
+                                       [0, 0, 0, 0, 0],
                                        [1, 1, 0, 0, 0],
                                        [1, 0, 0, 0, 0]],
                                       [2, 3, 4, 3]), "YES")
@@ -16,6 +16,26 @@ class TestGraph(unittest.TestCase):
     def test_Q42(self):
         self.assertEqual(Q42.solution(4, 3, [4, 1, 1]), 2)
         self.assertEqual(Q42.solution(4, 6, [2, 2, 3, 3, 4, 4]), 3)
+
+    def test_Q43(self):
+        self.assertEqual(Q43.solution(7, 11, [(0, 1, 7),
+                                              (0, 3, 5),
+                                              (1, 2, 8),
+                                              (1, 3, 9),
+                                              (1, 4, 7),
+                                              (2, 4, 5),
+                                              (3, 4, 15),
+                                              (3, 5, 6),
+                                              (4, 5, 8),
+                                              (4, 6, 9),
+                                              (5, 6, 11)]), 51)
+
+    def test_Q44(self):
+        self.assertEqual(Q44.solution(5, [(11, -15, -15),
+                                          (14, -5, -15),
+                                          (-1, -1, -5),
+                                          (10, -4, -1),
+                                          (19, -4, 19)]), 4)
 
 
 if __name__ == '__main__':
