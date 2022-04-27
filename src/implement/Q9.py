e72788b9acce8aa4efdf28477e103a94439f1377
gs-kikim <kikim@genians.com>
2022-04-27 20:56:58 +0900

    q9,10 implements commit

---
+++ src/implement/Q9.py
@@ -0,0 +1,25 @@
+"""
+https://programmers.co.kr/learn/courses/30/lessons/60057
+"""
+def solution(s):
+    chunks = len(s)
+    min_num = chunks
+
+    for chunk_size in range(1, chunks):
+        l = [s[i:i + chunk_size] for i in range(0, chunks, chunk_size)]
+        l.append("#")
+        w = l[0]
+        res = ""
+        cnt = 1
+        for word in l[1:]:
+            if w != word:
+                if cnt >= 2:
+                    res += w + str(cnt)
+                else:
+                    res += w
+                w = word
+                cnt = 1
+            else:
+                cnt += 1
+        min_num = min(min_num, len(res))
+    return min_num
--- test/test_implement.py
+++ test/test_implement.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.implement import Q7, Q8
+from src.implement import Q7, Q8, Q9, Q10
 
 
 class TestImplement(unittest.TestCase):
@@ -11,6 +11,19 @@ class TestImplement(unittest.TestCase):
     def test_Q8(self):
         self.assertEqual(Q8.solution("K1KA5CB7"), "ABCKK13")
         self.assertEqual(Q8.solution("AJKDLSI412K4JSJ9D"), "ADDIJJJKKLSS20")
+        self.assertEqual(Q8.solution("ABCDE"), "ABCDE")# ABCDE0
+
+    def test_Q9(self):
+        self.assertEqual(Q9.solution("aabbaccc"), 7)
+        self.assertEqual(Q9.solution("ababcdcdababcdcd"), 9)
+        self.assertEqual(Q9.solution("abcabcdede"), 8)
+        self.assertEqual(Q9.solution("abcabcabcabcdededededede"), 14)
+        self.assertEqual(Q9.solution("xababcdcdababcdcd"), 17)
+
+    def test_Q10(self):
+        self.assertEqual(Q10.solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],
+                                      [[1, 1, 1], [1, 1, 0], [1, 0, 1]]), True)
+
 
 
 if __name__ == '__main__':
