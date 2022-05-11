f1c04cadcaefabae0a6f01d1fb2e156412bad1b1
gs-kikim <kikim@genians.com>
2022-05-11 20:58:46 +0900

    q11 implements commit

--- test/test_implement.py
+++ test/test_implement.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.implement import Q7, Q8, Q9, Q10
+from src.implement import Q7, Q8, Q9, Q10, Q11
 
 
 class TestImplement(unittest.TestCase):
@@ -23,6 +23,14 @@ class TestImplement(unittest.TestCase):
     def test_Q10(self):
         self.assertEqual(Q10.solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],
                                       [[1, 1, 1], [1, 1, 0], [1, 0, 1]]), True)
+
+    def test_Q11(self):
+        self.assertEqual(Q11.solution(6, 3, [(3, 4), (2, 5), (5, 3)], 3, [(3, "D"), (15, 'L'), (17, 'D')]), 9)
+        self.assertEqual(
+            Q11.solution(10, 4, [(1, 2), (1, 3), (1, 4), (1, 5)], 4, [(8, "D"), (10, "D"), (11, "D"), (13, "L")]), 21)
+        self.assertEqual(
+            Q11.solution(10, 5, [(1, 5), (1, 3), (1, 2), (1, 6), (1, 7)], 4,
+                         [(8, "D"), (10, 'D'), (11, 'D'), (13, 'L')]), 13)
 
 
 
